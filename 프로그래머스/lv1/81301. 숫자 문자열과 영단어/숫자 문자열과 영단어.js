c75d67e2d390a7cf5d381cfc59d0ac8c4a502133
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-08-18 22:11:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -3,16 +3,14 @@ function solution(s) {
   	const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     let tmp = "";
     for(let x of s){
-      if(!isNaN(x)){
-        answer+=x;
-      }
+      if(!isNaN(x))answer+=x;
       else{
         tmp += x;
         if(numbers.includes(tmp)){
            answer += numbers.indexOf(tmp);
            tmp = "";
          }
-    	}
+        }   
     }
     return Number(answer);
 }
