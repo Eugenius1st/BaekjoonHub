8f7ab6f9dc950b911f79c10dddee8f49dab76b7d
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-08-24 22:00:24 +0900

    [level 1] Title: 소수 찾기, Time: 253.07 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,14 +1,4 @@
-function isPrime(x) {// 소수인지 판별하는 함수
-  for (let i = 2; i <= Math.sqrt(x); i++) {
-    if (x % i === 0) return false;
-  }
-  return true;
-}
 function solution(n) {
-  let answer = 0;
-  for (let i = 2; i <= n; i++) {
-    // 소수이면 소수의 개수에 1 추가
-    if (isPrime(i)) answer++;
-  }
-  return answer;
+    var answer = 0;
+    return answer;
 }
