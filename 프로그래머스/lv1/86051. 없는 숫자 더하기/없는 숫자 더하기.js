c3eb19f26843e5eb27b25eff839f462c5fe15dc9
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-08-25 20:25:54 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 4.90 ms, Memory: 30.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    var answer = 0;
+    const arr = Array.from({length:10},()=>1);
+    for(let x of numbers){
+        arr[x] = 0;
+    }
+    for(let y = 0 ; y <10;y++){
+        if(arr[y]===1) answer += y
+    }
+    console.log(arr)
+    return answer;
+}
