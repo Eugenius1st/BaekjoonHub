c4cbf44eb0f847c993ad631d2b8f24beb301da18
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-09-19 15:52:55 +0900

    [Silver IV] Title: 카드2, Time: 236 ms, Memory: 53160 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N = int(input())
+arr = list(range(1,N+1))
+arr = deque(arr)
+
+while len(arr) != 1:
+    arr.popleft()
+    arr.append(arr.popleft())
+
+print(arr[0])
