de59bb60c9eea964f8116ae1ee1b64e33bcd8fde
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-09-21 23:16:12 +0900

    [Bronze II] Title: 수 정렬하기, Time: 296 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,21 +1,21 @@
 import sys
 #from collections import deque
-# sys.stdin=open("input.txt", "rt")
+#sys.stdin=open("input.txt", "rt")
 arr = []
 n = int(input())
-# 버블정렬
+# 삽입정렬
 for i in range(n):
     tmp=int(input())
     arr.append(tmp)
 
-for i in range(n):
-    swap = "";
-    for j in range(0,n-1-i):
-        if(arr[j]>arr[j+1]):
-            swap = arr[j]
-            arr[j] = arr[j+1] 
-            arr[j+1] = swap
-    if swap =="": break
-
+for end in range(1,n):
+    i = end
+    while i > 0 and arr[i-1] > arr[i]:
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+        i -= 1
+    
 for x in arr:
     print(x)
+    
+# 삽입정렬
+   
