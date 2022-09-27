90b041375a251ec7f54970babb7b14bef20b337a
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-09-27 21:34:02 +0900

    [level 3] Title: 입국심사, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -0,0 +1,14 @@
+def solution(n,times):
+    start,end = 1,max(times)*n
+    res = end
+    while start <= end:
+        people = 0
+        mid = (start+end)//2
+        for time in times:
+            people += mid//time
+        if people < n:
+            start = mid+1
+        else:
+            end = mid-1
+            res = min(res,mid)
+    return res
