8502bf4b752e669e750652dccf3851441051f8b5
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-10-06 21:14:50 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 5584 ms, Memory: 32416 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,35 +1,16 @@
 import sys
+# BOJ__1158__덱
 from collections import deque
-
-# 큐를 이용할 것이다.
-# while 문의 array값이 false가 될 때까지
-# pop 과 append 반복하다가 k 번째에
-# 새로운 배열에 append 시킬 것이다
-
-n, k = map(int,input().split())
+N,K = map(int,input().split())
+arr= deque(list(range(1,N+1)))
 cnt = 0
-
-dQ = list(range(1,n+1))
-dQ = deque(dQ)
 res = []
-
-while dQ:
+while arr:
+    tmp = arr.popleft()
     cnt+=1
-    tmp = dQ.popleft()
-    if cnt==k:
-        res.append(tmp)
+    if cnt == K:
         cnt = 0
-    else:        
-        dQ.append(tmp)
-        
-
-print('<' + ', '.join(map(str, res)) + '>')
-#join 함수는 매개변수로 들어온 리스트에 있는 요소 하나하나를 합쳐서 하나의 문자열로 바꾸어 반환하는 함수입니다.
-# '구분자'.join(리스트)
-
-
-
-
-
-
-
+        res.append(tmp)
+    else:
+        arr.append(tmp)
+print("<"+", ".join(map(str,res))+">")
