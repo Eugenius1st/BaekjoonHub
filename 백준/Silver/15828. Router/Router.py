9866576214b2bdf8730abd980019ecb7cd626631
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-10-26 21:05:51 +0900

    [Silver IV] Title: Router, Time: 252 ms, Memory: 39132 KB -BaekjoonHub

---
+++ 백준/Silver/15828. Router/Router.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+def input():
+    return sys.stdin.readline().rstrip()
+packet = 0
+router_buffer = deque([0 for _ in range(packet)])
+bufferLen = int(input())
+
+while(packet != -1):
+    packet = int(input())
+    if packet == -1 : break
+
+    if packet == 0:
+        router_buffer.popleft()
+    else:
+        if len(router_buffer) >= bufferLen:continue
+        router_buffer.append(packet)
+    
+
+#print("\n".join(str(s) for s in router))
+#new_list = list(map(str, l1))
+print("\n".join(map(str, router_buffer)) if router_buffer else "empty")    
