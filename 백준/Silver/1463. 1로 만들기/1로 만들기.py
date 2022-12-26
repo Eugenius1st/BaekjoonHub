b91c033daba1d59ab17e5b4672c47bd239dc5912
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2022-12-26 23:01:18 +0900

    [Silver III] Title: 1로 만들기, Time: 264 ms, Memory: 38232 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -3,10 +3,16 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
-N = int(input())
-dp=[0]*(N+1)
-for i in range(2,N+1):
-    dp[i]=dp[i-1]+1
-    if i % 2 == 0 and dp[i]>dp[i//2]+1: dp[i]=dp[i//2]+1
-    if i % 3 == 0 and dp[i]>dp[i//3]+1: dp[i]=dp[i//3]+1
-print(dp[N])
+def findFunc(val):
+    for i in range(2,N+1):
+        stack[i]=stack[i-1]+1
+        if i % 2 ==0 and stack[i]> stack[i//2]+1:
+            stack[i]=stack[i//2]+1
+        if i % 3 ==0 and stack[i]> stack[i//3]+1:
+            stack[i]=stack[i//3]+1
+    print(stack[val])
+
+if __name__ == "__main__":
+    N = int(input())
+    stack = [0]*(N+1)
+    findFunc(N)
