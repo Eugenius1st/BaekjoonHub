5bd2aaeaede9658894c34ba07885d9bb62cbe552
Eugenius1st <84161508+Eugenius1st@users.noreply.github.com>
2026-04-07 14:14:59 +0900

    [level 2] Title: 프로세스, Time: 0.72 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,22 @@
+from collections import deque 
+# 중요도가 높은 문서를 먼저 인쇄    
+def solution(priorities, location):
+    idxs = deque([i for i in range(len(priorities))])
+    priorities = deque(priorities)
+    ans = []
+    
+    cnt = 0
+    while priorities:
+        tmp = priorities.popleft()
+        idxTmp = idxs.popleft()
+        
+        maxTmp = max(priorities) if len(priorities) > 0 else 0
+        if maxTmp <= tmp:
+            cnt += 1
+            if idxTmp == location:
+                return cnt
+        else: 
+            priorities.append(tmp)
+            idxs.append(idxTmp)
+            
+    return
